994819a7db8dd6f3a95ed87bbf41e4913ebed6e0
Богдан <bohdansalo31@gmail.com>
2025-01-05 20:13:35 +0200

    ffff

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,69 @@
 "use strict";
-let userName = "Vlad"; //string
-const counter = "10"; //number
+// let userName = "Vlad"; //string
+// const counter = 10; //number
 
-let isLogin = true; //boolean
+// let isLogin = true; //boolean
 
-let age; //undefine
-const userChoice = null; //null
+// let age; //undefine
+// const userChoice = null; //null
 
-const bigNumber = 10n; //bigint
+// const bigNumber = 10n; //bigint
 
-const key = Symbol("mykey"); //Symbol
+// const key = Symbol("mykey"); //Symbol
 
 // alert(welcomeMessage);
 
-const isUserstayWebSite = confirm("Залишитись на сайті?");
+// const isUserstayWebSite = confirm("Залишитись на сайті?");
 
-console.log(isUserstayWebSite);
+// console.log(isUserstayWebSite);
+
+// const userAge = prompt("Вкажіть свій вік");
+
+// const str = "Привіт";
+// const num = 123;
+// const flag = true;
+// const txt = "true";
+
+// console.log("в змінні str тип данних:", typeof str);
+// console.log("змінній num тип данних:", typeof num);
+// console.log("в змінній flag тип данних:", typeof flag);
+// console.log("в змінній txt тип данних:", typeof txt);
+
+// const cardNumber = "1234 5678 1234 5678";
+// const userAge = 18;
+// const isRed = true;
+// const bigNumber = 24n;
+// const cart = null;
+
+// let userName = "Bohdan";
+// let price = 199;
+// let age;
+// let id = Symbol();
+// let userEmail = "bohdansalo31@gmail.com";
+
+// const myAge = 15;
+// const myFriendAge = 17;
+// const sumOfAge = myAge + myFriendAge;
+// console.log("сумма мого віку та мого друга:", sumOfAge);
+
+// const a = 1488;
+// const b = 2017;
+// const product = a * b;
+// console.log("Добуток :", product);
+
+// const group = "FE_39_23_Students_G1";
+// console.log("Я навчаюся в GoITeens в групі:", group);
+
+// const yourName = prompt("Яке ваше ім'я?");
+// console.log(yourName);
+
+// const name = "MacBook pro 13";
+// let price = 100000;
+// console.log("Обрано:", name, "ціна за штуку:", price);
+// price = 20000;
+
+const product = "IPhone 15";
+let quantity = 5;
+console.log("на складі є", product, "у кількості", quantity);
+quantity = 10;
+console.log("на складі є", product, "у кількості", quantity);
