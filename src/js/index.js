55a46b7ec477212a5268816e3dd894ebdb29922f
Богдан <bohdansalo31@gmail.com>
2025-01-05 18:21:35 +0200

    isuserwebsite

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+"use strict";
+let userName = "Vlad"; //string
+const counter = "10"; //number
+
+let isLogin = true; //boolean
+
+let age; //undefine
+const userChoice = null; //null
+
+const bigNumber = 10n; //bigint
+
+const key = Symbol("mykey"); //Symbol
+
+// alert(welcomeMessage);
+
+const isUserstayWebSite = confirm("Залишитись на сайті?");
+
+console.log(isUserstayWebSite);
