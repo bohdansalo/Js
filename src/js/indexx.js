29eaf9fe08ec7eafda83cb83064c119f1ab9e0e1
Богдан <bohdansalo31@gmail.com>
2025-01-07 21:16:54 +0200

    first commit twoo

---
+++ src/js/indexx.js
@@ -0,0 +1,61 @@
+// console.log(2 ** 5); //степені
+// console.log(2 + 7); // додавання
+// console.log(2 - 4); // віднімання
+// console.log(2 * 9); //Множення
+// console.log(4 / 10); //Ділення
+// console.log(4 % 2); //остача від ділення
+
+// let num = 10;
+// // console.log((num += 5));
+// num += 2;
+// console.log(num);
+
+// console.log(1 > 2); //Більше
+// console.log(1 < 2); //Мешьше
+// console.log(1 == 2); //Рівність
+// console.log(1 === 2); //Сувора рівність
+// console.log(2 !== 1); //не рівність
+// console.log(1 >= 0);
+
+// // 1 завдання
+// let a1 = 5 + 3;
+// let a2 = 5 - 3;
+// let a3 = 5 * 3;
+// let a4 = 5 / 3;
+
+// console.log("Змінна а1 результат:", a1);
+// console.log("Змінна а2 результат:", a2);
+// console.log("Змінна а3 результат:", a3);
+// console.log("Змінна а4 результат:", a4);
+
+// // завдання 2
+// const a5 = 5 % 3;
+// const a6 = 3 % 5;
+// const a7 = 5 + "3";
+// const a8 = "5" - 3;
+// const a9 = 75 + "kg";
+
+// alert(a5);
+// alert(a6);
+// alert(a7);
+// alert(a8);
+// alert(a9);
+// // завдання 3
+
+// const min = 1;
+// const max = 10;
+// const randomNumber = Math.random() * (max - min) + min;
+// console.log(randomNumber);
+// // завдання 4
+// const firstNumber = Number.parseFloat(promt("Введіть перше число"));
+// const secondNumber = Number.parseFloat(promt("Введіть друге число"));
+// const sym = firstNumber + secondNumber;
+// alert("Сумма" + sum);
+
+// завдання 3.2
+
+let heightCircus = 10;
+const diameterCircus = 4;
+
+const volumCircus = Marh.Pi * heightCircus * diameterCircus;
+console.log("Обьєм циліндру:" * volumCircus, toFixed(2), "Метрів кубічних");
